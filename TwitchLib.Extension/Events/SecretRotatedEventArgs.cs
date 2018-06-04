71f34eba9014edbca4994c6bee9ff6608484d04b
Prom3theu5 <dave.sekula@gmail.com>
2018-06-04 17:29:29 +0100

    SecretRotated Event, and Args

---
+++ TwitchLib.Extension/Events/SecretRotatedEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TwitchLib.Extension.Events
+{
+    public class SecretRotatedEventArgs : EventArgs
+    {
+        public string NewSecret { get; set; }
+        public DateTime Expires { get; set; }
+
+    }
+}
--- TwitchLib.Extension/Extension/RotatedSecretExtension.cs
+++ TwitchLib.Extension/Extension/RotatedSecretExtension.cs
@@ -1,18 +1,19 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
-using TwitchLib.Extension.Models;
+using TwitchLib.Extension.Events;
 
 namespace TwitchLib.Extension
 {
     public class RotatedSecretExtension : ExtensionBase
     {
         private readonly int _interval;
+        public event EventHandler<SecretRotatedEventArgs> SecretRotated;
 
         public RotatedSecretExtension(ExtensionConfiguration config, int rotationIntervalMinutes = 720) : base(config)
         {
             _interval = rotationIntervalMinutes * 1000 * 60;
             var secrets = GetExtensionSecretAsync().GetAwaiter().GetResult();
-
             if (secrets != null)
                 Secrets = secrets.Secrets.ToList();
 
@@ -22,22 +23,26 @@ namespace TwitchLib.Extension
                 {
                     await Task.Delay(_interval);
 
-                    //Until saving of Extension Config is implemented, just get current secret so it can't be lost!
-                    //CreateAndSaveNewSecret(await CreateExtensionSecretAsync());
+                    if (SecretRotated != null)
+                    {
+                        secrets = await CreateExtensionSecretAsync();
+                        if (secrets == null) return;
 
-                    CreateAndSaveNewSecret(await GetExtensionSecretAsync());
+                        Secrets = secrets.Secrets.ToList();
+                        var currentSecret = Secrets.ToList().OrderByDescending(x => x.Expires).First();
+                        OnSecretRotated(new SecretRotatedEventArgs
+                        {
+                            NewSecret = currentSecret.Content,
+                            Expires = currentSecret.Expires
+                        });
+                    }
                 }
             });
         }
 
-        private void CreateAndSaveNewSecret(ExtensionSecrets extensionSecrets)
+        protected virtual void OnSecretRotated(SecretRotatedEventArgs e)
         {
-            if (extensionSecrets == null) return;
-
-            // Todo: Save Secret Here - Where?
-            // EF allowing end-developer specified providers?
-
-            Secrets = extensionSecrets.Secrets.ToList();
+            SecretRotated?.Invoke(this, e);
         }
     }
 }
